fbda1734528266c8b5225e9f27590df6248079e6
Eyal Mishor <eyal@vcita.com>
2018-08-04 14:11:59 +0300

    deploy

---
+++ js/about.2b8d2e12.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"container"},[n("h2",[e._v("This is my first Vue.js web app !!")])])}],i=n("2877"),r={},c=Object(i["a"])(r,s,a,!1,null,null,null);t["default"]=c.exports}}]);
+//# sourceMappingURL=about.2b8d2e12.js.map
